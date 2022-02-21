9212de0b872caf7c239529025a488ff7ff219659
Matt.Wang <mattwang44@gmail.com>
2022-02-21 22:35:55 +0800

    chore: add flake8 config and fix coding style based on flake8 warnings

--- registry/admin.py
+++ registry/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Entry
 
-# Register your models here.
+
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('key', 'value', 'type', 'enabled', 'comment', )
