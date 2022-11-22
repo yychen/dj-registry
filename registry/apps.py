0b32aa70df6c1add3bb3772b100ec16eaeaa6983
Tom Chen <tom@fugu.ooo>
2022-11-22 13:31:16 +0800

    feat: add support to django 4

--- registry/apps.py
+++ registry/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class RegistryConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'registry'
     verbose_name = "Django Registry"
